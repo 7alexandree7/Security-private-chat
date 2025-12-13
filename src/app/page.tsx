3d7703e457516f8148bdcd26100c2bb3e92852b0
alexandre <alexandre020602@gmail.com>
2025-12-13 12:58:39 -0300

    UX/UI inicial + gerar nome + id e salvar no localStorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,68 @@
-export default function Home() {
-  return (
-    <div>
+"use client"
 
-    </div>
+import { nanoid } from "nanoid"
+import { useEffect, useState } from "react"
+
+
+export default function Home() {
+
+  const ANIMALS = ["wolf", "hawkk", "bear", "shark"]
+  const STORAGE_KEY = "chat_username"
+
+  const [userName, setUserName] = useState<string | null>("")
+
+  const generateUserName = () => {
+    const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)]
+    return `Anonymous-${word}-${nanoid(5)}`
+  }
+
+  useEffect(() => {
+    const main = () => {
+      const stored = localStorage.getItem(STORAGE_KEY)
+      if (stored) {
+        setUserName(stored)
+      }
+      else {
+        const generate = generateUserName()
+        localStorage.setItem(STORAGE_KEY, generate)
+        setUserName(generate)
+      }
+    }
+    main()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md space-y-8">
+
+        <div className="text-center space-y-2">
+          <h1 className="text-2xl font-bold tracking-tight text-green-500">{">"}Private_chat</h1>
+          <p className="text-zinc-500 text-sm">A private, self-destructing chat room.</p>
+        </div>
+
+        <div className="border border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md">
+          <div className="space-y-5 ">
+            <div className="space-y-2">
+              <label className="flex items-center text-zinc-500">Your Identity</label>
+              <div className="flex items-center gap-3">
+                <div className="flex-1 bg-zinc-950 border border-zinc-800 p-3 text-sm text-zinc-400 font-mono">
+                  {userName}
+                </div>
+              </div>
+            </div>
+
+            <button
+              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold
+             hover:bg-zinc-50 hover:text-black transition-colors cursor-pointer disabled:opacity-50"
+            >
+              CREATE SECURE ROOM
+            </button>
+
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
