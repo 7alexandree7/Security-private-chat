9fa646d8828117c06bca01a33a05f8dc258ac17a
alexandre <alexandre020602@gmail.com>
2025-12-15 03:03:56 -0300

    config provider react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/components/providers";
 
 const JetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -18,11 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" suppressHydrationWarning>
+    <html lang="pt-br">
       <body
+        suppressHydrationWarning
         className={`${JetBrainsMono.variable}  antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
